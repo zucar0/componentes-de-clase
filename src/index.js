9e7a5abe129bda7d2586a022b88e3ed7a7fa36e5
zucar0 <42826452+zucar0@users.noreply.github.com>
2022-12-31 14:11:10 -0600

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-import App from './App';
+class App extends Component {
+  render() {
+    console.log(this.props);
+    return <p>Hola, componente de clase</p>;
+  }
+}
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+render(<App />, document.getElementById('root'));
